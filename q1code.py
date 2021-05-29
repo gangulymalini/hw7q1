a4e6c1670140ba77951dbf7c0451e5ad26c77bfb
Malini Ganguly <gangulym@oregonstate.edu>
2021-05-29 15:02:39 -0700

    code has been written, test has been passed!

--- q1code.py
+++ q1code.py
@@ -1,3 +1,13 @@
 def fizzBuzz():
-    print("hi")
-    return "yay"
+    teststring = ""
+    for i in range (1,101):
+        if ((i % 3) == 0):
+            if ((i % 5) == 0):
+                teststring = teststring + "FizzBuzz "
+            else:
+                teststring = teststring + "Fizz "
+        elif ((i % 5) == 0):
+                teststring = teststring + "Buzz "
+        else:
+            teststring = teststring + str(i) + " "
+    return(teststring)
